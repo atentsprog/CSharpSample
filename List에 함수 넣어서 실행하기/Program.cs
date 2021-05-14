f7fe3e4dc33167f5e56c959068764f3d3432ab2c
hktrollgames <hktrollgames@gmail.com>
2021-05-14 22:42:29 +0900

    리스트에 함수 넣어서 실행하는 샘플

---
+++ List에 함수 넣어서 실행하기/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List에_함수_넣어서_실행하기
+{
+    class Program
+    {
+        static Random random = new Random();
+        static List<Action<Player, Monster>> attackActions = new List<Action<Player, Monster>>();
+        static void Main(string[] args)
+        {
+            attackActions.Add(NormalAttack);
+            attackActions.Add(SkillAttack);
+
+            Player player = new Player();
+            Monster monster = new Monster();
+
+            int attackIndex = random.Next(0, attackActions.Count);
+            attackActions[attackIndex](player, monster);
+            attackActions.RemoveAt(attackIndex); // 사용한 어택 삭제.
+
+
+
+            attackIndex = random.Next(0, attackActions.Count);
+            attackActions[attackIndex](player, monster);
+            attackActions.RemoveAt(attackIndex); // 사용한 어택 삭제.
+        }
+        static public void NormalAttack(Player player, Monster monster)
+        {
+            Console.WriteLine("플레이어가 몬스터를 일반 공격");
+        }
+        static public void SkillAttack(Player player, Monster monster)
+        {
+            Console.WriteLine("플레이어가 몬스터를 스킬공격");
+        }
+    }
+
+    class Monster
+    {
+        public int power;
+        public int hp;
+    }
+
+    class Player
+    {
+        public int power;
+        public int hp;
+    }
+}
